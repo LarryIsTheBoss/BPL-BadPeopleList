00d976c1a08330e64f3c077c90c89aff18a01317
LarryTheBoss <larrytmoore1@gmail.com>
2020-08-21 23:22:23 -0500

    IP address and port is now properly split.
    Player's IP address and port is now properly split so no matter what port they are on the code will run fine and no error will be thrown.

--- lua/autorun/server/core.lua
+++ lua/autorun/server/core.lua
@@ -6,6 +6,8 @@ This should easily deter most people thrown on the list.
 
 Addon Creator: QuirkyLarry - STEAM_0:0:80555260
 ]]
+
+-- Localized function(s).
 local l_hAd = hook.Add
 local l_IVd = IsValid
 local l_tHV = table.HasValue
@@ -14,7 +16,7 @@ local l_mTe = math.Truncate
 local l_pnt = print
 local l_tCe = timer.Create
 local l_tsg = tostring
-local l_sgb = string.gsub
+local l_sEe = string.Explode
 local l_ie = include
 local l_tRe = timer.Remove
 l_ie("bpl_config.lua")
@@ -25,20 +27,21 @@ l_hAd("PlayerInitialSpawn", "BPL_SoftDetectionSystem", function(ply)
   local TimeUntilDisconnect = l_mrm(BPL.MinTimeBeforePunish,BPL.MaxTimeBeforePunish)
   local l_SteamID64 = ply:SteamID64()
   local l_IPAddress = ply:IPAddress()
-  local l_IPAddressSimple = l_sgb(l_IPAddress, ":27005", "") or l_sgb(l_IPAddress, ":27006", "") or l_sgb(l_IPAddress, ":27007", "") or l_sgb(l_IPAddress, ":27008", "")
   local l_PlayerName = ply:Nick()
+  -- Splits the IP Address and the port number they are connected on.
+  l_IPAddressSimple = l_sEe(":", l_IPAddress)
   l_mTe(TimeUntilDisconnect, 0)
   -- Creates a timer to punish the player - SteamID64 Check.
   if l_tHV(BPL.BPL_SteamID64M, l_SteamID64) or l_tHV(BPL.BPL_SteamID64CB, l_SteamID64) then
       l_tCe("BPL_Punish_" .. l_SteamID64 .. "_Custom_Timer", TimeUntilDisconnect, 1, function()
           if not SoftPunishID then ply:SendLua( 'while true do end' ); ply:Kick("Steam auth ticket has been cancelled") else ply:Kick("Steam auth ticket has been cancelled") end
-          l_pnt("[BPL] | " .. l_PlayerName .. " - " .. l_SteamID64 .. " attempted to play but was caught. - Their current IP: " .. l_IPAddressSimple .. " - Detection Method: [SteamID64] | " .. "IP Blacklisted: " .. l_tsg(l_tHV(BPL.BPL_IPs, l_IPAddressSimple)))
+          l_pnt("[BPL] | " .. l_PlayerName .. " - " .. l_SteamID64 .. " attempted to play but was caught. - Their current IP: " .. l_IPAddressSimple[1] .. " - Detection Method: [SteamID64] | " .. "IP Blacklisted: " .. l_tsg(l_tHV(BPL.BPL_IPs, l_IPAddressSimple[1])))
       end)
-  elseif l_tHV(BPL.BPL_IPs, l_IPAddressSimple) then
+  elseif l_tHV(BPL.BPL_IPs, l_IPAddressSimple[1]) then
       -- Creates a timer to punish the player - IP Check.
       l_tCe("BPL_Punish_" .. l_SteamID64 .. "_Custom_Timer", TimeUntilDisconnect, 1, function()
           if not SoftPunishIP then ply:SendLua( 'while true do end' ); ply:Kick("Steam auth ticket has been cancelled") else ply:Kick("Steam auth ticket has been cancelled") end
-          l_pnt("[BPL] | " .. l_PlayerName .. " - " .. l_SteamID64 .. " attempted to play but was caught. - Their current IP: " .. l_IPAddressSimple .. " - Detection Method: [IPAddress] | " .. "SteamID Blacklisted: " .. l_tsg(l_tHV(BPL.BPL_SteamID64M, l_SteamID64) or l_tHV(BPL.BPL_SteamID64CB, l_SteamID64)))
+          l_pnt("[BPL] | " .. l_PlayerName .. " - " .. l_SteamID64 .. " attempted to play but was caught. - Their current IP: " .. l_IPAddressSimple[1] .. " - Detection Method: [IPAddress] | " .. "SteamID Blacklisted: " .. l_tsg(l_tHV(BPL.BPL_SteamID64M, l_SteamID64) or l_tHV(BPL.BPL_SteamID64CB, l_SteamID64)))
       end)
   end
 end)
